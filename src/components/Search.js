d50bb7ca8978df81df90d048baf914578aaf1ab4
Patrick Sheriddan <sheridp2@gmail.com>
2020-11-10 11:05:42 -0800

    search results list rendering to page

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,42 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const Search = () =>{
-  const [term, setTerm] = useState('')
+  const [term, setTerm] = useState('React')
+  const [results, setResults] = useState([])
+
+
 
   useEffect(() =>{
+    const search = async () =>{
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php',{
+        params:{
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
 
-  } )
+        },
+      })
+      setResults(data.query.search)
+    }
+
+    search()
+  }, [term])
+
+  const renderedResults = results.map((result) =>{
+    return(
+      <div className="item">
+        <div className= "content">
+          <div className="header">
+            {result.title}
+          </div>
+            {result.snippet}
+        </div>
+      </div>
+
+    )
+  })
 
   return (
     <div>
@@ -18,6 +49,9 @@ const Search = () =>{
             onChange={e=> setTerm(e.target.value)}
             className="input" />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   )
