60cbcf55f5a36860f21c13e8501dfa9ecc03abf0
Patrick Sheriddan <sheridp2@gmail.com>
2020-11-10 15:22:15 -0800

    refactored Serach code to remove error

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,18 @@ import axios from 'axios';
 
 const Search = () =>{
   const [term, setTerm] = useState('React')
+  const [debouncedTerm, setDebouncedTerm] = useState(term)
   const [results, setResults] = useState([])
 
+  useEffect(() => {
+    const timerId = setTimeout(() =>{
+      setDebouncedTerm(term);
+    }, 1000)
 
+    return () =>{
+      clearTimeout(timerId)
+    }
+  }, [term])
 
   useEffect(() =>{
     const search = async () =>{
@@ -15,24 +24,16 @@ const Search = () =>{
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term
+          srsearch: debouncedTerm,
 
         },
       })
       setResults(data.query.search)
     }
+    search()
+  }, [debouncedTerm])
 
-    const timeoutId = setTimeout(() =>{
-      if(term){
-        search()
-      }
-    }, 750)
 
-    return () =>{
-      clearTimeout(timeoutId)
-    }
-
-  }, [term])
 
   const renderedResults = results.map((result) =>{
     return(
